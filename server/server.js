f32027074a0a4dc991c4ffef3137e0677e55567a
Abhishek <abhishekys4570@gmail.com>
2026-03-13 02:12:02 +0530

    fix cors hardcode vercel origin

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173', process.env.FRONTEND_URL];
+const allowedOrigins = ['http://localhost:5173', 'https://mern-auntentication.vercel.app'];
 
 app.use(express.json());
 app.use(cookieParser());
